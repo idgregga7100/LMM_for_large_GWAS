e2b03626ebf68e81545d01dee079923c059154e7
idgregga7100 <isadominica@gmail.com>
2024-03-24 16:00:43 -0500

    fixing subsampling

--- plink_to_vcf.sh
+++ plink_to_vcf.sh
@@ -2,3 +2,4 @@
 #convert from plink to vcf format
 ./plink2 --bfile /home/igregga/LMM_files/2500simu-genos --export vcf --out /home/igregga/LMM_files/vcfs/2500simu-genos
 ./plink2 --bfile /home/igregga/LMM_files/1250simu-genos --export vcf --out /home/igregga/LMM_files/vcfs/1250simu-genos
+
