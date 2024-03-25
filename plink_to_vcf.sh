8e476f4c2175e166204cfc856a5aa59c840c87c5
idgregga7100 <isadominica@gmail.com>
2024-03-25 15:09:18 -0500

    edit vcf conversion

--- plink_to_vcf.sh
+++ plink_to_vcf.sh
@@ -2,4 +2,5 @@
 #convert from plink to vcf format
 ./plink2 --bfile /home/igregga/LMM_files/2500simu-genos --export vcf --out /home/igregga/LMM_files/vcfs/2500simu-genos
 ./plink2 --bfile /home/igregga/LMM_files/1250simu-genos --export vcf --out /home/igregga/LMM_files/vcfs/1250simu-genos
+./plink2 --bfile /home/igregga/LMM_files/5000simu-genos --export vcf --out /home/igregga/LMM_files/vcfs/5000simu-genos
 
