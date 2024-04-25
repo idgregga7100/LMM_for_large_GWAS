19dd2c97216cfd12b1f242d5aee0c6fa2cf7b1b8
Taylor Fischer <tfischer1@luc.edu>
2024-04-25 17:54:56 -0500

    adding pheno files

--- test/run_BOLT_test.sh
+++ test/run_BOLT_test.sh
@@ -7,16 +7,14 @@
 # Run from directory 'test' in main LMM_for_large_GWAS directory!
 
 ## FOR ARGPARSING!
-#full path to bed/bim/fam directory: /home/igregga/LMM_files/
-geno_dir=/home/igregga/LMM_files/
-#prefix of bed/bim/fam files
-geno_prefix=/home/igregga/LMM_files/1250simu-genos
-#full path to phenotypes (with header row containing FID and IID!)
-pheno=/home/igregga/LMM_files/phenos/simu_categorical.phen 
+#path & prefix of bed/bim/fam files
+geno_prefix=100simu-genos
+#path to phenotypes (with header row containing FID and IID!)
+pheno=simu_categorical-01na.phen
 #name of column in pheno file containing the phenotype
 pheno_col=TRAIT
 #prefix for output file name, will be <prefix>.tab
-out_prefix=1250simu-genos_bolt
+out_prefix=100simu-genos_cc_bolt
 #number of threads to use
 threads=2
 
@@ -34,9 +32,6 @@ cwd=$(pwd)
 
 # base command
 # ./bolt --bfile=geno --phenoFile=pheno.txt --phenoCol=phenoName --lmm --LDscoresFile=tables/LDSCORE.1000G_EUR.tab.gz --statsFile=stats.tab
-
-# change to directory with input genotype files
-#cd $geno_dir
 
 # command to compute for categorical or continuous, including time and memory for benchmarking
 time /usr/bin/time --verbose $cwd/../../BOLT-LMM_v2.4.1/bolt \
@@ -65,9 +60,8 @@ echo "BOLT-LMM for ${geno_prefix} is complete."
 
 # no need to specify cc or qt, runs the same for both!
 # need to take in:
-    # full path to directory containing bam/bim/fam files for $geno_dir
-    # prefix of bam/bim/fam plink format files for $geno_prefix (passed to --bfile)
-    # pheno file path/name (full path!) for $pheno (passed to --phenoFile)
+    # path and prefix of bam/bim/fam plink format files for $geno_prefix (passed to --bfile)
+    # pheno file path/name for $pheno (passed to --phenoFile)
     # column name of column containing phenotype for $pheno_col (passed to --phenoCol)
     # prefix to name output file (passed to --statFile)
     # ?maybe number of threads for $threads (passed to --numThreads)
