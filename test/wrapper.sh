6b2f5a23b191d4dcc728661fcd40667ee38ae77e
idgregga7100 <isadominica@gmail.com>
2024-04-26 11:55:22 -0500

    NOT RUNABLE but how do we feel about this idea?

---
+++ test/wrapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#okay here's my idea. set up, like, absolute args for this script
+#edit output path arg with string manip for each tool
+#be able to take the output path formula to easily call sumstat scripts for plotting
+#??????? how do we feel about this
+#for example
+input=plink
+pheno=whatev
+output=test_out
+
+outbolt=${output}_bolt
+
+run_bolt -o outbolt
+
+rscript -e 'rmarkdown::render(\"plot.rmd\",params=list(bolt=outbolt))'
+#not 100% on how passing a bash variable to the rendering command should work
